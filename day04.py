40927b9494e0cb4f3680a3815ab56831f9b936c0
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-12-05 20:33:47 -0800

    Cleaned up days 4 and 5

--- day04.py
+++ day04.py
@@ -7,8 +7,7 @@ import collections
 
 from get_input import get_input, line_parser
 
-def part1(guard_log):
-    """Solution to part 1"""
+def make_guard_dict(guard_log):
     guard_dict = collections.defaultdict(list)
     current_guard = None
     is_asleep = False
@@ -30,8 +29,14 @@ def part1(guard_log):
             guard_dict[current_guard].append((time, tuple(start), tuple(end)))
         else:
             raise Exception(f"Unrecognized message {message}")
+    return guard_dict
+
+
+def part1(guard_log):
+    """Solution to part 1"""
+    guard_dict = make_guard_dict(guard_log)
     most_rested, times = max(guard_dict.items(), key=lambda kv: sum(t[0] for t in kv[1]))
-    
+
     time_counter = collections.defaultdict(int)
     for hour, minute in itertools.product(range(24), range(60)):
         for _, start, end in times:
@@ -42,32 +47,14 @@ def part1(guard_log):
 
 def part2(guard_log):
     """Solution to part 2"""
-    guard_dict = collections.defaultdict(list)
-    current_guard = None
-    is_asleep = False
-    for year, month, day, hour, minute, guard, message in sorted(guard_log, key=lambda g: g[:5]):
-        if guard is not None:
-            assert not is_asleep
-            current_guard = guard
-            is_asleep = False
-        elif message == "falls asleep":
-            assert not is_asleep
-            is_asleep = True
-            start = (year, month, day, hour, minute)
-        elif message == "wakes up":
-            assert is_asleep
-            is_asleep = False
-            end = (year, month, day, hour, minute)
-            assert start[:3] == end[:3]
-            time = 60*(end[3] - start[3])+end[4]-start[4]
-            guard_dict[current_guard].append((time, tuple(start), tuple(end)))
-        else:
-            raise Exception(f"Unrecognized message {message}")
+    guard_dict = make_guard_dict(guard_log)
     time_counter = collections.defaultdict(int)
     for hour, minute in itertools.product(range(24), range(60)):
         for guard, times in guard_dict.items():
             for _, start, end in times:
-                if start[3] <= hour < end[3] or (start[3] == end[3] and start[4] <= minute < end[4]) and not (hour == end[3] and minute == end[4]):
+                if start[3] <= hour < end[3] or\
+                        (start[3] == end[3] and start[4] <= minute < end[4]) and\
+                        not (hour == end[3] and minute == end[4]):
                     time_counter[(guard, hour, minute)] += 1
     guard, _ = max(time_counter.items(), key=lambda kv: kv[1])
     return guard[0] * guard[2]
--- day05.py
+++ day05.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env pipenv run python
 """Solutions to day 5 of Advent of Code"""
 
-import re
-import itertools
-import collections
+from get_input import get_input
 
-from get_input import get_input, line_parser
+def reduce(polymer):
+    """Reduce string as much as possible by removing matching letter pairs
+    A matching pair is one uppercase letter and one lowercase
+    example:
+        abBa => aa
+    """
+    new_polymer = tuple(polymer)
+    polymer = None
+    while not new_polymer == polymer:
+        polymer = tuple(new_polymer)
+        new_polymer = ()
+        for p in polymer:
+            if new_polymer and new_polymer[-1] != p and new_polymer[-1].lower() == p.lower():
+                new_polymer = new_polymer[:-1]
+            else:
+                new_polymer += (p,)
+    return polymer
+
 
 def part1(polymer):
     """Solution to part 1"""
-    old_polymer = None
-    while not old_polymer == polymer:
-        old_polymer = tuple(polymer)
-
+    return len(reduce(polymer))
 
 def part2(polymer):
     """Solution to part 2"""
-    ...
+    minimum = None
+    for p in set(l.lower() for l in list(polymer)):
+        processed = polymer.replace(p.upper(), '').replace(p, '')
+        p_len = len(reduce(processed))
+        if minimum is None or p_len < minimum:
+            minimum = p_len
+    return minimum
 
 
 if __name__ == '__main__':
-    LINE = get_input(day=5, year=2018)
+    LINE = get_input(day=5, year=2018).strip()
     print("Part 1: {}".format(part1(LINE)))
     print("Part 2: {}".format(part2(LINE)))
