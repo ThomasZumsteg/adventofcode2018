7ef589cb377bd914aad81ec0590433ee998e1014
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2019-02-02 23:37:28 -0800

    making progress

--- day17.py
+++ day17.py
@@ -14,22 +14,15 @@ class Point(collections.namedtuple("Point", ["x", "y"])):
             return NotImplemented
         return type(self)(self.x + other.x, self.y + other.y)
 
-    def __lt__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x < other.x and self.y < other.y
-
-    def __gt__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return not (self < other or self == other)
+Point.UP, Point.DOWN = Point(0, -1), Point(0, 1)
+Point.LEFT, Point.RIGHT = Point(1, 0), Point(-1, 0)
 
 
 class Mapping(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.reverse_mapping = collections.defaultdict(set)
-        self._water_front = set()
+        self.water_front = set()
 
     @classmethod
     def make_mapping(cls, rows):
@@ -48,7 +41,7 @@ class Mapping(dict):
         return Point(
             min((p for p in self.keys()), key=lambda p: p.x).x,
             min((p for p in self.keys()), key=lambda p: p.y).y)
-    
+
     def __str__(self):
         lines = []
         smallest, largest = self.min(), self.max()
@@ -57,7 +50,7 @@ class Mapping(dict):
             for x in range(smallest.x, largest.x+1):
                 lines[-1] += self.get(Point(x, y), '.')
         return '\n'.join(lines)
-    
+
     def __getitem__(self, key):
         if isinstance(key, Point):
             return self.get(key, '.')
@@ -68,7 +61,7 @@ class Mapping(dict):
             raise ValueError(f"Key must be a Point, not {key}")
         super().__setitem__(key, value)
         self.reverse_mapping[value].add(key)
-    
+
     def __delitem__(self, key):
         val = self[key]
         self.reverse_mapping[val].discard(key)
@@ -83,23 +76,76 @@ class Mapping(dict):
         return new
 
     def take_step(self):
-        queue = set((*self['+'], *self['|'], *self['~']))
-        for space in queue:
-            if self[space + Point(0, -1)] == '.' and not (self.min() < space < self.max()):
-                import pdb; pdb.set_trace()
+        if self.water_front == set():
+            self.water_front.update(self['+'])
+        new_self = self.copy()
+        new_self.water_front = set()
+        for space in self.water_front:
+            assert isinstance(space, Point)
+            if not self.in_bounds(space):
+                continue
+
+            if self[space] == '+':
+                if self[space + Point.DOWN] == '.':
+                    new_self[space + Point.DOWN] = '|'
+                    new_self.water_front.add(space + Point.DOWN)
+                else:
+                    raise NotImplementedError("No empty space below source")
+            elif self[space] == '|':
+                if self[space + Point.DOWN] == '.':
+                    new_self[space + Point.DOWN] = '|'
+                    new_self.water_front.add(space + Point.DOWN)
+                else:
+                    boundary = new_self.fill_row(space)
+                    assert boundary != set()
+                    new_self.water_front.update(boundary)
+            else:
+                raise NotImplementedError("What do now?")
+        return new_self
+
+    def in_bounds(self, other):
+        lower_left = self.min()
+        upper_right = self.max()
+        return lower_left.x <= other.x and lower_left.y <= other.y and \
+                upper_right.x >= other.x and upper_right.y >= other.y
+
+    def fill_row(self, space):
+        queue = [(Point.LEFT, space), (Point.RIGHT, space)]
+        row = set()
+        boundery = set()
+        tracebacks = set()
+
+        while queue:
+            direction, point = queue.pop(0)
+            if self[point] == '#':
+                continue
+            if self[point + Point.UP] == '|':
+                tracebacks.add(point + Point.UP)
+            row.add(point)
+            if self[point + Point.DOWN] == '.':
+                boundery.add(point)
+            else:
+                queue.append((direction, point + direction))
+        char, return_val = '|', tuple(boundery)
+        if boundery == set():
+            char = '~'
+            return_val = tracebacks
+        for item in row:
+            self[item] = char
+        assert return_val != set()
+        return return_val
 
 def part1(clay):
     """Solution to part 1"""
-    new_water_map = Mapping.make_mapping(clay)
-    new_water_map[Point(500, 0)] = '+'
-    old_water_map = None
-    assert new_water_map[next(iter(new_water_map['+'])) + Point(0, 1)] == '.'
-    while old_water_map != new_water_map:
-        old_water_map = new_water_map
-        new_water_map = old_water_map.take_step()
+    water_map = Mapping.make_mapping(clay)
+    water_map[Point(500, 0)] = '+'
+    water_map.water_front = set((Point(500, 0),))
+    while water_map.water_front != set():
+        water_map = water_map.take_step()
+    import pdb; pdb.set_trace()
     count = 0
     for value in ('~', '|'):
-        count += len(new_water_map[value])
+        count += len(water_map[value])
     return count
 
 def part2(tests):
@@ -109,9 +155,9 @@ def part2(tests):
 
 def parse(line):
     match = re.match(r"([xy])=(\d+), ([xy])=(\d+)..(\d+)", line)
-    return tuple(int(n) if n.isdigit() else n for n in match.groups() )
+    return tuple(int(n) if n.isdigit() else n for n in match.groups())
 
 if __name__ == '__main__':
-    clay = line_parser(get_input(day=17, year=2018), parse=parse)
-    print("Part 1: {}".format(part1(clay)))
-    print("Part 2: {}".format(part2(clay)))
+    clay_map = line_parser(get_input(day=17, year=2018), parse=parse)
+    print("Part 1: {}".format(part1(clay_map)))
+    print("Part 2: {}".format(part2(clay_map)))
