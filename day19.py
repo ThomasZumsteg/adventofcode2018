56db0192df2ddd4cda2e6e81176670bce3d8889f
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2019-02-05 23:49:18 -0800

    Solution to day 19

--- day19.py
+++ day19.py
@@ -9,7 +9,7 @@ from get_input import get_input
 Instruction = collections.namedtuple('Instruction', ('instr', 'reg'))
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARNING)
 
 fh = logging.FileHandler('day19.log', mode='w+')
 fh.setFormatter(logging.Formatter('%(message)s'))
@@ -23,10 +23,16 @@ log.addHandler(ch)
 
 def make_func(func):
     def wrapped(reg, val):
-        val = list(val)
+        val = val.copy()
         val[reg[2]] = func(reg, val)
-        return tuple(val)
+        return val
     return wrapped
+
+def factors(num):
+    for n in range(1, int(num ** 0.5) + 1):
+        if num % n == 0:
+            yield n
+            yield num // n
 
 funcs = {
     'addr': make_func(lambda reg, val: val[reg[0]] + val[reg[1]]),
@@ -51,24 +57,33 @@ def part1(*, ip, program):
     """Solution to part 1"""
     values = [0, 0, 0, 0, 0, 0]
     while 0 <= values[ip] < len(program):
+        if values[ip] == 1:
+            # SHORT CUT!
+            break
         line = program[values[ip]]
-        new = funcs[line.instr](line.reg, values)
-        log.debug(
-            "[%6d,%6d,%6d,%6d,%6d,%6d] %4s %2d %2d %2d [%6d,%6d,%6d,%6d,%6d,%6d]",
-            *values, line.instr, *line.reg, *new)
-        values = list(new)
+        old_ip = values[ip]
+        values = funcs[line.instr](line.reg, values)
+        log.debug("%2d %4s %2d %2d %2d [%6d,%6d,%6d,%6d,%6d,%6d]",
+                  old_ip, line.instr, *line.reg, *values)
         values[ip] += 1
-    return values[0]
+    log.info('====================DONE====================')
+    return sum(factors(values[1]))
 
 def part2(*, ip, program):
-    """Solution to part 2"""
+    """Solution to part 2, based on static analysis"""
     values = [1, 0, 0, 0, 0, 0]
     while 0 <= values[ip] < len(program):
+        if values[ip] == 1:
+            # SHORT CUT!
+            break
         line = program[values[ip]]
+        old_ip = values[ip]
         values = funcs[line.instr](line.reg, values)
-        values = list(values)
+        log.debug("%2d %4s %2d %2d %2d [%6d,%6d,%6d,%6d,%6d,%6d]",
+                  old_ip, line.instr, *line.reg, *values)
         values[ip] += 1
-    return values[0]
+    log.info('====================DONE====================')
+    return sum(factors(values[1]))
 
 def parse(text):
     lines = iter(text.splitlines())
@@ -90,8 +105,59 @@ seti 8 0 4
 seti 9 0 5"""
 
 if __name__ == '__main__':
-    args = parse(test)
-    part1(**args)
     args = parse(get_input(day=19, year=2018))
     print("Part 1: {}".format(part1(**args)))
     print("Part 2: {}".format(part2(**args)))
+
+
+"""
+p1_limit = 915
+p2_limit = 10551315
+total = 0
+for i in range(limit):
+    for j in range(limit):
+        if i * j == limit:
+            total += i
+return total
+"""
+
+"""
+#ip 2
+0  addi 2 16 2 # goto 16
+1  seti 1 1 3 # for r3 in range(r1)
+    2  seti 1 7 5 # for r5 in range(r1)
+        3  mulr 3 5 4
+        4  eqrr 4 1 4 # if r3 * r5 == r1 add r3 to r0
+            5  addr 4 2 2 # jump r4
+            6  addi 2 1 2 # goto 7
+            7  addr 3 0 0 # r3 + r0 => r0
+    8  addi 5 1 5 # for r5 in range(1, r1):
+    9  gtrr 5 1 4 # if r5 > r1 goto 2 else r3++
+    10 addr 2 4 2 # jump r4
+    11 seti 2 3 2 # goto 2
+    12 addi 3 1 3 # r3++
+13 gtrr 3 1 4     # if r3 > r1 EXIT else goto 1
+    14 addr 4 2 2 # jump r4
+    15 seti 1 9 2 # goto 1
+    16 mulr 2 2 2 # EXIT
+# sum(j for i in range(1, r1) for j in range(1, r1) if i * j == r1)
+17 addi 1 2 1
+18 mulr 1 1 1
+19 mulr 2 1 1
+20 muli 1 11 1
+21 addi 4 3 4
+22 mulr 4 2 4
+23 addi 4 13 4
+24 addr 1 4 1
+25 addr 2 0 2 # jump r0
+26 seti 0 1 2 # goto 0
+27 setr 2 0 4
+28 mulr 4 2 4
+29 addr 2 4 4
+30 mulr 2 4 4
+31 muli 4 14 4
+32 mulr 4 2 4
+33 addr 1 4 1
+34 seti 0 4 0
+35 seti 0 5 2
+"""
