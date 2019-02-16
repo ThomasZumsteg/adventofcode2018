1c6be417a984525e61ced74203ee249812ce2be7
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2019-02-15 22:51:52 -0800

    Solution to part2

--- day23.py
+++ day23.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pipenv run python
 
 import collections
+import itertools
 import re
 
 from get_input import get_input, line_parser
@@ -26,6 +27,13 @@ def bots_in_range(position, radius, bots):
         if position.distance(bot.position) <= radius:
             yield bot
 
+def bots_in_box_range(position, radius, bots):
+    total = 0
+    for bot in bots:
+        if position.distance(bot.position) <= radius + bot.radius:
+            total += 1
+    return total
+
 def part1(bots):
     """Solution to part 1"""
     big_bot = max(bots, key=lambda b: b.radius)
@@ -33,8 +41,34 @@ def part1(bots):
 
 def part2(bots):
     """Solution to part 2"""
-    pass
+    position = Point(0, 0, 0)
+    max_radius = 1
+    while bots_in_box_range(position, max_radius, bots) < len(bots):
+        max_radius *= 2
 
+    positions = set((position,))
+    radius = max_radius
+    while radius > 0:
+        radius //= 2
+        new_positions = set()
+        seen = set()
+        best_count = None
+        for position in positions:
+            for diff in itertools.chain(
+                    ((0, 0, 0),),
+                    itertools.product(*((0, radius, -radius), ) * 3)):
+                new_position = Point(*diff) + position
+                if new_position in seen:
+                    continue
+                seen.add(new_position)
+                new_count = bots_in_box_range(new_position, radius, bots)
+                if best_count is None or new_count > best_count:
+                    new_positions.clear()
+                    best_count = new_count
+                if new_count == best_count:
+                    new_positions.add(new_position)
+        positions = new_positions
+    return min(p.distance(Point(0, 0, 0)) for p in positions)
 
 PART1_TEST = """pos=<0,0,0>, r=4
 pos=<1,0,0>, r=1
@@ -64,3 +98,4 @@ if __name__ == '__main__':
     print("Part 1: {}".format(part1(bot_list)))
     assert part2(line_parser(PART2_TEST, parse=parse)) == 36
     print("Part 2: {}".format(part2(bot_list)))
+    # 111960222
