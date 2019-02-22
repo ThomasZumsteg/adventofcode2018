4b2ab9e302ab14859672f8df880c17f88e9a73e6
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2019-02-21 22:26:41 -0800

    Solution part 13, done with python solutions

--- day13.py
+++ day13.py
@@ -1,92 +1,107 @@
 #!/usr/bin/env pipenv run python
 
 import collections
-from dataclasses import dataclass
+import copy
 
-from get_input import get_input, line_parser
+from get_input import get_input
 
-@dataclass
-class Point:
-    x: int
-    y: int
-
-    def __hash__(self):
-        return hash((self.x, self.y))
+class Point(collections.namedtuple("Point", ["x", "y"])):
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
-DIRECTIONS = {
-    'v': Point(0, 1),
-    '>': Point(1, 0),
-    '<': Point(-1, 0),
-    '^': Point(0, -1)
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Cart:
+    _DIRECTIONS = {
+        'v': Point(0, 1),
+        '>': Point(1, 0),
+        '<': Point(-1, 0),
+        '^': Point(0, -1)
+        }
+
+    _TURNS = {
+        ('v', '/'): '<', ('v', '\\'): '>',
+        ('>', '/'): '^', ('>', '\\'): 'v',
+        ('^', '/'): '>', ('^', '\\'): '<',
+        ('<', '/'): 'v', ('<', '\\'): '^',
     }
 
-TURNS = {
-    ('v', '/'): '<', ('v', '\\'): '>',
-    ('>', '/'): '^', ('>', '\\'): 'v',
-    ('^', '/'): '>', ('^', '\\'): '<',
-    ('<', '/'): 'v', ('<', '\\'): '^',
-}
+    _TURN_MAP = ('>', 'v', '<', '^')
 
-def intersection(cart, n_turn):
-    turn_map = ('>', 'v', '<', '^')
-    n_turn %= 3
-    current = turn_map.index(cart)
-    r_val = cart
-    if n_turn == 0:
-        r_val = turn_map[(current - 1)]
-    elif n_turn == 2:
-        r_val = turn_map[(current + 1) % len(turn_map)]
-    return r_val
+    def __init__(self, direction, position):
+        self.direction = direction
+        self.position = position
+        self._turns = 0
 
-def print_current(pos, track, cart=None):
-    grid = [list((line[(pos.x-1 if pos.x > 0 else 0):pos.x+2]))
-            for line in track[(pos.y-1 if pos.y > 0 else 0):pos.y+2]]
-    if cart is not None:
-        grid[1][1] = cart
-    print(''.join('\n' + ''.join(row) for row in grid))
+    def __repr__(self):
+        return f"Cart({self.direction}, {self.position})"
+
+    def __str__(self):
+        return self.direction
+
+    def move(self):
+        self.position += self._DIRECTIONS[self.direction]
+
+    def turn(self, segment):
+        if segment == '+':
+            self._turns %= 3
+            current = self._TURN_MAP.index(self.direction)
+            if self._turns == 0:
+                self.direction = self._TURN_MAP[(current - 1)]
+            elif self._turns == 2:
+                self.direction = self._TURN_MAP[(current + 1) % len(self._TURN_MAP)]
+            self._turns += 1
+        else:
+            self.direction = self._TURNS.get(
+                (self.direction, segment),
+                self.direction)
+
+    def collision(self, other):
+        return self.position == other.position and self != other
+
+    def __lt__(self, other):
+        return (self.position.x, self.position.y) < (other.position.x, other.position.y)
+
+
+def print_track(track, carts):
+    track = [list(row) for row in track]
+    for cart in carts:
+        track[cart.position.y][cart.position.x] = cart.direction
+    print(''.join('\n' + ''.join(row) for row in track))
 
 
 def part1(track, carts):
     """Solution to part 1"""
+    carts = copy.deepcopy(carts)
     while True:
-        old_carts, carts = carts, {}
-        for old_pos, (cart, turn) in old_carts.items():
-            pos = old_pos + DIRECTIONS[cart]
-            if pos in carts or (old_pos in carts and pos in old_carts):
-                return f"{pos.x},{pos.y}"
-            track_segment = track[pos.y][pos.x]
-            if track_segment == '+':
-                cart = intersection(cart, turn % 3)
-                turn += 1
-            else:
-                cart = TURNS.get((cart, track_segment), cart)
-            carts[pos] = (cart, turn)
+        carts.sort()
+        for cart in carts:
+            cart.move()
+            segment = track[cart.position.y][cart.position.x]
+            cart.turn(segment)
+            if any(c.collision(cart) for c in carts):
+                return f"{cart.position.x},{cart.position.y}"
 
 def part2(track, carts):
     """Solution to part 2"""
-    output = open('output_13.failing', 'w')
+    # output = open('output_13.failing', 'w')
+    carts = set(copy.deepcopy(carts))
     while len(carts) > 1:
-        old_carts, carts = carts, {}
-        for old_pos, (cart, turn) in old_carts.items():
-            pos = old_pos + DIRECTIONS[cart]
-            if pos in carts:
-                del carts[pos]
-                continue
-            elif old_pos in carts:
-                del carts[old_pos]
-                continue
-            track_segment = track[pos.y][pos.x]
-            if track_segment == '+':
-                cart = intersection(cart, turn % 3)
-                turn += 1
-            else:
-                cart = TURNS.get((cart, track_segment), cart)
-            carts[pos] = (cart, turn)
-        output.write('[' + ', '.join(sorted([f"({pos.x},{pos.y})" for pos in carts])) + ']\n')
-    pos = list(carts.keys())[0]
+        for cart in sorted(carts):
+            cart.move()
+            segment = track[cart.position.y][cart.position.x]
+            cart.turn(segment)
+            for c in list(carts):
+                if c.collision(cart):
+                    carts.remove(c)
+                    carts.remove(cart)
+                    break
+    pos = list(carts)[0].position
     return f"{pos.x},{pos.y}"
 
 Record = collections.namedtuple('Record', 'input output')
@@ -95,12 +110,12 @@ def make_track(lines):
     lines = tuple(lines.splitlines())
     cart_types = {'v': '|', '>': '-', '^': '|', '<': '-'}
     track = []
-    carts = {}
+    carts = []
     for y, row in enumerate(lines):
         track.append([])
         for x, char in enumerate(row):
             if char in cart_types:
-                carts[Point(x, y)] = (char, 0)
+                carts.append(Cart(char, Point(x, y)))
                 char = cart_types[char]
             assert char in ' |\\/+-'
             track[-1].append(char)
@@ -124,8 +139,8 @@ TEST_TRACK_2 = """/>-<\
 
 if __name__ == '__main__':
     assert '7,3' == part1(*make_track(TEST_TRACK_1))
-    assert '6,4' == part2(*make_track(TEST_TRACK_2))
     TRACK, CARTS = make_track(get_input(day=13, year=2018))
     print("Part 1: {}".format(part1(TRACK, CARTS)))
     # NOT (58,119), (
+    assert '6,4' == part2(*make_track(TEST_TRACK_2))
     print("Part 2: {}".format(part2(TRACK, CARTS)))
