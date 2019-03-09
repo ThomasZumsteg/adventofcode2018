61655e0f02c7c446d7dd46eac5276bc99cd32a88
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2019-03-08 18:43:00 -0800

    Solution to day 7 in rust

--- day07.rs
+++ day07.rs
@@ -5,18 +5,58 @@ use regex::Regex;
 
 use std::collections::{HashMap, HashSet};
 
-type Input = HashMap<String, Vec<String>>;
+type Input = HashMap<String, HashSet<String>>;
 
-fn part1(input: &Input) -> u32 {
-    let mut roots: HashSet<&String> = input.keys().collect();
-    for value in input.values().flat_map(|v| v.iter()) {
-        roots.remove(value);
+fn part1(input: &Input) -> String {
+    let mut todo = input.clone();
+    let mut result: Vec<String> = Vec::new();
+    while !todo.is_empty() {
+        let mut to_remove: Vec<String> = Vec::new();
+        for (key, values) in todo.clone() {
+            if values.iter().all(|v| !todo.contains_key(v)) {
+                to_remove.push(key);
+            }
+        }
+        to_remove.sort();
+        todo.remove(&to_remove[0]);
+        result.push(to_remove[0].clone());
     }
-    unimplemented!()
+    return result.join("");
 }
 
 fn part2(input: &Input) -> u32 {
-    unimplemented!()
+    let mut todo = input.clone();
+    let mut time = 0;
+    let mut workers: Vec<(u32, String)> = Vec::new();
+    while !todo.is_empty() {
+        let t_diff = workers.iter().map(|v| v.0).min().unwrap_or(0) + 1;
+        time += t_diff;
+        workers = workers.iter().filter_map( |w| {
+            if w.0 <= t_diff {
+                todo.remove(&w.1);
+                None
+            } else {
+                Some((w.0 - t_diff, w.1.clone()))
+            }
+        }).collect();
+        let mut ready: Vec<String> = Vec::new();
+        for key in todo.keys() {
+            if todo.values().any(|vals| vals.contains(key)) {
+                continue;
+            } else if workers.iter()
+                .any(|v| &v.1 == key) {
+                continue
+            }
+            ready.push(key.clone());
+        }
+        ready.sort();
+        while workers.len() < 5 && !ready.is_empty() {
+            let item = ready.remove(0);
+            let first = item.chars().next().unwrap() as u8;
+            workers.push(((first - 'A' as u8 + 60) as u32, item.clone()));
+        }
+    }
+    time
 }
 
 fn parse(lines: &String) -> Input {
@@ -25,9 +65,10 @@ fn parse(lines: &String) -> Input {
         .unwrap();
     for line in lines.trim().split('\n') {
         let groups = format.captures(line).unwrap(); 
+        result.entry(groups[1].to_string()).or_insert(HashSet::new());
         result.entry(groups[2].to_string())
-            .or_insert(Vec::new())
-            .push(groups[1].to_string());
+            .or_insert(HashSet::new())
+            .insert(groups[1].to_string());
     }
     return result
 }
