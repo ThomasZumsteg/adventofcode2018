491513a2e2d19377e412cc95f4a92dd539eca6a1
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2019-04-12 17:11:26 -0700

    Solution to day 14 in rust

--- day14.rs
+++ day14.rs
@@ -1,13 +1,13 @@
 use common::get_input;
 
-fn digits(number: u8) -> Vec<u8> {
+fn digits(number: u32) -> Vec<u8> {
     if number == 0 {
         return vec![0];
     }
     let mut result = Vec::new();
     let mut remainer = number;
     while remainer > 0 {
-        result.insert(0, remainer % 10);
+        result.insert(0, (remainer % 10) as u8);
         remainer /= 10;
     }
     result
@@ -18,7 +18,7 @@ fn part1(rounds: u32) -> String {
     let mut elf1 = 0;
     let mut elf2 = 1;
     loop {
-        let digits = digits(recipies[elf1] + recipies[elf2]);
+        let digits = digits((recipies[elf1] + recipies[elf2]) as u32);
         for d in digits {
             recipies.push(d);
             if 10 + rounds <= recipies.len() as u32 {
@@ -35,20 +35,15 @@ fn part2(input: u32) -> String {
     let mut recipies = vec![3u8, 7];
     let mut elf1 = 0;
     let mut elf2 = 1;
-    let mut count = 0;
-    let target: Vec<u8> = input.to_string().chars().map(|d| d.to_string().parse::<u8>().unwrap()).collect();
-    let target_len: usize = target.len();
+    let target: Vec<u8> = digits(input);
     loop {
-        let digits = digits(recipies[elf1] + recipies[elf2]);
-        count += 1;
+        let digits = digits((recipies[elf1] + recipies[elf2]) as u32);
         for d in digits {
             recipies.push(d);
-            if recipies.len() > 10 {
-                let digits = recipies[recipies.len()-target_len..recipies.len()].to_vec(); 
-                // println!("{}: {:?} == {:?}", count, target, digits);
-                if digits == target {
-                    return count.to_string();
-                }
+            if recipies.len() < target.len() { continue }
+            let digits = recipies[recipies.len()-target.len()..recipies.len()].to_vec(); 
+            if digits == target {
+                return (recipies.len() - target.len()).to_string();
             }
         }
         elf1 = (recipies[elf1] as usize + 1 + elf1) % recipies.len();
@@ -61,10 +56,6 @@ fn parse(input: String) -> u32 {
 }
 
 fn main() {
-    assert!(part1(5) == "0124515891");
-    assert!(part1(9) == "5158916779");
-    assert!(part1(18) == "9251071085");
-    assert!(part1(2018) == "5941429882");
     let input = parse(get_input(14, 2018));
     println!("Part 1: {}", part1(input));
     println!("Part 2: {}", part2(input));
