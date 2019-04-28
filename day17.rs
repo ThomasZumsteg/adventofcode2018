8d868179c234cfad97527329b06d54c82dedaa8a
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2019-04-27 22:55:51 -0700

    With debugging

--- day17.rs
+++ day17.rs
@@ -7,22 +7,22 @@ use common::get_input;
 
 type Input = HashMap<Point, char>;
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Clone, Copy, Eq, PartialEq, Hash)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
 }
 
 impl Point {
-    fn new(x: u32, y: u32) -> Point {
+    fn new(x: i32, y: i32) -> Point {
         Point { x, y }
     }
 }
 
-impl Add for &Point {
+impl Add for Point {
     type Output = Point;
 
-    fn add(self, other: &Point) -> Point {
+    fn add(self, other: Point) -> Point {
         Point::new(self.x + other.x, self.y + other.y)
     }
 }
@@ -34,28 +34,68 @@ impl fmt::Debug for Point {
 }
 
 struct WaterMap<'a> {
-    _map: &'a Input,
-    _state: Input,
-    _lower_left: Point, 
-    _upper_right: Point, 
+    map: &'a Input,
+    state: Input,
+    front: Vec<Point>,
+    lower_right: Point, 
+    upper_left: Point, 
+}
+
+impl <'a>fmt::Debug for WaterMap<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut result = String::new();
+        for y in self.upper_left.y..self.lower_right.y {
+            for x in self.upper_left.x..self.lower_right.x {
+                result.push(self.get(&Point::new(x, y)));
+            }
+            result.push('\n');
+        }
+        write!(f, "{}", result)
+    }
 }
 
 impl <'a>WaterMap<'a> {
     fn new(map: &'a Input) -> WaterMap<'a> {
         let mut water = HashMap::new();
         water.insert(Point{x:500, y:0}, '+');
+        let max_y = map.keys().map(|p| p.y).max().unwrap();
+        let min_x = map.keys().map(|p| p.x).min().unwrap();
+        let max_x = map.keys().map(|p| p.x).max().unwrap();
         WaterMap {
-            _map: map,
-            _state: water,
-            _lower_left: Point{ x:0, y:0 },
-            _upper_right: Point{ x:0, y:0 },
+            map: map,
+            state: water,
+            upper_left: Point{ x: min_x, y: 0 },
+            lower_right: Point{ x: max_x+1, y: max_y+1 },
+            front: Vec::new(),
         }
+    }
+
+    fn get(&self, point: &Point) -> char {
+        if let Some(c) = self.state.get(&point) { *c }
+        else if let Some(c) = self.map.get(&point) { *c }
+        else { '.' }
+    }
+
+    fn step(&mut self) {
+        let mut next = Vec::new();
+        for p in self.front.clone() {
+            let below = p + Point::new(0, 1);
+            match self.get(&below) {
+                '#' => unimplemented!(),
+                '~' => unimplemented!(),
+                '|' => unimplemented!(),
+                '.' => unimplemented!(),
+                err => panic!("Unknow code {:?} as point {:?}", err, p),
+            }
+        }
+        self.front = next;
     }
 }
 
 
 fn part1(input: &Input) -> u32 {
     let map = WaterMap::new(input);
+    println!("{:?}", map);
     unimplemented!()
 }
 
@@ -74,7 +114,7 @@ fn parse(lines: String) -> Input {
                 result.insert(Point{x, y}, '#');
             }
         } else {
-            let y = cap[1].parse().unwrap();
+            let y = cap[2].parse().unwrap();
             for x in cap[4].parse().unwrap()..cap[5].parse().unwrap() {
                 result.insert(Point{x, y}, '#');
             }
