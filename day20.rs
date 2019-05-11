09ee8b3062558aa2b824b21180fdc343763207d9
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2019-05-11 11:19:26 -0700

    Start on day 20 in rust

--- day20.rs
+++ day20.rs
@@ -1,9 +1,25 @@
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap, VecDeque};
+use std::iter::FromIterator;
 
 use common::{get_input, map};
 use common::point::Point;
 
-type Input = HashMap<Point, Point>;
+type Input = HashMap<Point, HashSet<Point>>;
+
+fn map_distances(map: Input) -> HashMap<Point, usize> {
+    let mut queue = VecDeque::from_iter(vec![(Point::new(0, 0), 0)]);
+    let mut door_counts: HashMap<Point, usize> = HashMap::new();
+    while let Some((position, doors)) = queue.pop_front() {
+        if door_counts.contains_key(&position) {
+            continue
+        }
+        door_counts.insert(position, doors);
+        for next_position in map.get(&position).unwrap().iter() {
+            queue.push_front((next_position.clone(), doors + 1));
+        }
+    }
+    unimplemented!()
+}
 
 fn part1(input: &Input) -> usize {
     unimplemented!()
@@ -14,15 +30,37 @@ fn part2(input: &Input) -> usize {
 }
 
 fn parse(text: String) -> Input {
-    let directions = map! {
-        "N" => Point::new(0, 1),
-        "S" => Point::new(0, -1),
-        "E" => Point::new(1, 0),
-        "W" => Point::new(-1, 0)
-    };
-    let location = Point::new(0, 0);
+    let mut location = Point::new(0, 0);
     let mut door_map: Input = HashMap::new();
-    unimplemented!()
+    let mut stack = VecDeque::new();
+    let mut queue = VecDeque::from_iter(text.trim().chars());
+    assert!(queue.pop_front() == Some('^'));
+    assert!(queue.pop_back() == Some('$'));
+    while let Some(dir) = queue.pop_front() {
+        match dir {
+            '(' => { stack.push_front(location); },
+            ')' => { location = stack.pop_front().unwrap(); },
+            '|' => { location = stack.front().unwrap().clone(); },
+            _ => {
+                let next_location = location + match dir {
+                    'N' => Point::new(0, 1),
+                    'S' => Point::new(0, -1),
+                    'E' => Point::new(1, 0),
+                    'W' => Point::new(-1, 0),
+                    _ => panic!("What is this?"),
+                };
+                door_map.entry(next_location)
+                    .or_insert(HashSet::new())
+                    .insert(location);
+                door_map.entry(location)
+                    .or_insert(HashSet::new())
+                    .insert(next_location);
+                location = next_location;
+            },
+        }
+    }
+    assert!(stack.len() == 0);
+    door_map
 }
 
 fn main() {
--- lib.rs
+++ lib.rs
@@ -51,16 +51,3 @@ pub mod point {
         }
     }
 }
-
-#[macro_export]
-macro_rules! map(
-    { $($key:expr => $value:expr),+ } => {
-        {
-            let mut m = HashMap::new();
-            $(
-                m.insert($key, $value);
-            )+
-            m
-        }
-    }
-);
